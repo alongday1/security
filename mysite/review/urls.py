3b1f18915d83684bf01f36e9c88eb32e778d08f5
Sumireko-Usami <128973237+Sumireko-Usami@users.noreply.github.com>
2024-11-05 14:25:19 +0800

    Update urls.py

--- mysite/review/urls.py
+++ mysite/review/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
 
     path("comments/submit_form/<int:dish_id>/", views.submit_form, name='submit_form'),
     path("comments/submit_rating/<int:dish_id>/", views.submit_rating, name='submit_rating'),
-    path("comments/update_like/", views.update_like, name='update_like'),
     path("comments/update_dish_info/", views.update_dish_info, name='update_dish_info'),
     path("comments/add_image/", views.add_image, name='add_image'),
     path("comments/refresh_comments/", views.refresh_comments, name='refresh_comments'),
